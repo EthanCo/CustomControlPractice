6661bbe617b910bf64802b8419c06e68b91982cd
EthanCo <fenghenyu@163.com>
2016-09-05 16:20:23 +0800

    MyScrollView

--- MyAVLoadingindicator/app/src/main/java/com/ethanco/myavloadingindicator/MainActivity.java
+++ MyAVLoadingindicator/app/src/main/java/com/ethanco/myavloadingindicator/MainActivity.java
@@ -9,6 +9,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         //setContentView(R.layout.activity_main);
-        setContentView(R.layout.activity_test);
+        //setContentView(R.layout.activity_test);
+        setContentView(R.layout.layout_myscroll);
     }
 }
---
+++ MyAVLoadingindicator/app/src/main/java/com/ethanco/myavloadingindicator/MyScrollView.java
@@ -0,0 +1,127 @@
+package com.ethanco.myavloadingindicator;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.util.DisplayMetrics;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.widget.Scroller;
+
+/**
+ * Created by EthanCo on 2016/9/5.
+ */
+public class MyScrollView extends ViewGroup {
+
+    private int mScreenheight;
+    private Scroller mScroller;
+    private int mLastY;
+    private int mStart;
+    private int mEnd;
+
+    public MyScrollView(Context context) {
+        super(context);
+        initView(context);
+    }
+
+    public MyScrollView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        initView(context);
+    }
+
+    public MyScrollView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        initView(context);
+    }
+
+    private void initView(Context context) {
+        WindowManager wm = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
+        DisplayMetrics dm = new DisplayMetrics();
+        wm.getDefaultDisplay().getMetrics(dm);
+        mScreenheight = dm.heightPixels;
+        mScroller = new Scroller(context);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        int count = getChildCount();
+        //测量所有子View
+        for (int i = 0; i < count; i++) {
+            View childView = getChildAt(i);
+            measureChild(childView, widthMeasureSpec, heightMeasureSpec);
+        }
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int l, int t, int r, int b) {
+        int childCount = getChildCount();
+        //设置ViewGroup的高度
+        MarginLayoutParams mlp = (MarginLayoutParams) getLayoutParams();
+        mlp.height = mScreenheight * childCount;
+        setLayoutParams(mlp);
+
+        //layout所有的子View
+        for (int i = 0; i < childCount; i++) {
+            View child = getChildAt(i);
+            if (child.getVisibility() != View.GONE) {
+                child.layout(l, i * mScreenheight, r, (i + 1) * mScreenheight);
+            }
+        }
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        int y = (int) event.getY();
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                mLastY = y;
+                mStart = getScrollY();
+                break;
+            case MotionEvent.ACTION_MOVE:
+                if (!mScroller.isFinished()) {
+                    mScroller.abortAnimation();
+                }
+                int dy = mLastY - y;
+                if (getScrollY() < 0) {
+                    dy = 0;
+                }
+                if (getScrollY() > getHeight() - mScreenheight) {
+                    dy = 0;
+                }
+                scrollBy(0, dy);
+                mLastY = y;
+                break;
+            case MotionEvent.ACTION_UP:
+            case MotionEvent.ACTION_CANCEL:
+                mEnd = getScrollY();
+                int dScrollY = mEnd - mStart;
+                if (dScrollY > 0) {
+                    if (dScrollY < mScreenheight / 3) {
+                        mScroller.startScroll(0, getScrollY(), 0, -dScrollY);
+                    } else {
+                        mScroller.startScroll(0, getScrollY(), 0, mScreenheight - dScrollY);
+                    }
+                } else {
+                    if (-dScrollY < mScreenheight / 3) {
+                        mScroller.startScroll(0, getScrollY(), 0, -dScrollY);
+                    } else {
+                        mScroller.startScroll(0, getScrollY(), 0, -mScreenheight - dScrollY);
+                    }
+                }
+                break;
+        }
+        postInvalidate();
+        return true;
+    }
+
+    @Override
+    public void computeScroll() {
+        super.computeScroll();
+        if (mScroller.computeScrollOffset()) {
+            scrollTo(0, mScroller.getCurrY());
+            postInvalidate();
+        }
+    }
+}
